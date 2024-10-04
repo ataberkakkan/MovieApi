275270c93fe22eaf5480b49ae00c9083ac37a063
ataberkakkan <akkanataberk@gmail.com>
2024-10-04 11:07:23 +0300

    File Handling

--- src/main/java/com/movieflix/movieApi/dtos/MovieDto.java
+++ src/main/java/com/movieflix/movieApi/dtos/MovieDto.java
@@ -1,45 +1,34 @@
-package com.movieflix.movieApi.entities;
+package com.movieflix.movieApi.dtos;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Set;
 
-@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-public class Movie {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class MovieDto {
     private int movieId;
 
-    @Column(nullable = false, length = 200)
     @NotBlank(message = "Title cannot be empty!")
     private String title;
 
-    @Column(nullable = false)
     @NotBlank(message = "Director cannot be empty!")
     private String director;
 
-    @Column(nullable = false)
     @NotBlank(message = "Studio cannot be empty!")
     private String studio;
 
-    @ElementCollection
-    @CollectionTable(name = "movie_cast")
     private Set<String> movieCast;
 
-    @Column(nullable = false)
     private int releaseYear;
 
-    @Column(nullable = false)
     @NotBlank(message = "Poster cannot be empty!")
     private String poster;
+
+    @NotBlank(message = "Poster URL cannot be empty!")
+    private String posterUrl;
 }
